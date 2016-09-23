ca5ad5df1d8bd21cecf89fae13973b95a7a71e91
David Abrahams <davidabrahams@sbcglobal.net>
2016-09-23 19:04:55 -0400

    Adding Order and Customer

---
+++ lab1/Lab1/app/src/main/java/com/david/lab1/Customer.java
@@ -0,0 +1,15 @@
+package com.david.lab1;
+
+/**
+ * Created by david on 9/23/16.
+ */
+
+public class Customer {
+
+    private String name;
+
+    public Customer(String n) {
+        name = n;
+    }
+
+}
---
+++ lab1/Lab1/app/src/main/java/com/david/lab1/Order.java
@@ -0,0 +1,16 @@
+package com.david.lab1;
+
+import android.view.Menu;
+
+import java.util.ArrayList;
+
+/**
+ * Created by david on 9/23/16.
+ */
+
+public class Order {
+
+    private Customer customer;
+    private ArrayList<MenuItem> items;
+
+}
